f931355b679a9c91590c819577da631a2f0bc5f6
FRANCO <parisifrancoo@gmail.com>
2025-11-28 01:11:47 -0300

    BubbleSort edit

--- ip-2c2025-alumnos-main/visualizador/algorithms/sort_bubble.py
+++ ip-2c2025-alumnos-main/visualizador/algorithms/sort_bubble.py
@@ -2,22 +2,48 @@
 
 items = []
 n = 0
-i = 0
-j = 0
+i = 0 #pasada actua
+j = 0 #posicion dentro de la pasada
 
 def init(vals):
-    global items, n, i, j
-    items = list(vals)
+    global items, n, i, j 
+    items = list(vals) #guarda la lista
     n = len(items)
-    i = 0
-    j = 0
+    i = 0 #numero de pasada
+    j = 0 #posicion de comparacion
 
 def step():
-    # TODO:
+    global items, n, i, j
+
+    if i >= n - 1:
+        return{"done": True}    # Cuando no queden pasos, devolvé {"done": True}. 
+    #NO SE PUEDE PONER AL FINAL ESTE CONDICIONAL, PORQUE LOS DEMAS RETURN LO ANULARIAN ANTES.
+
     # 1) Elegir índices a y b a comparar en este micro-paso (según tu Bubble).
+
+    a = j
+    b = j + 1
+
     # 2) Si corresponde, hacer el intercambio real en items[a], items[b] y marcar swap=True.
+
+    swap = False
+
+    if items[a] > items[b]:
+        aux = items[a]
+        items[a] = items[b]
+        items[b] = aux
+        swap = True
+
     # 3) Avanzar punteros (preparar el próximo paso).
+
+    j = j + 1
+
+    # Si llegamos al final de la pasada, reiniciar j y avanzar la pasada i
+
+    if j >= n - 1 - i: #Le resta las posiciones que ya están ordenadas al final, i se usa para ignorar los valores que quedaron al final de la lista en la actual pasada. 
+        j = 0
+        i = i + 1
+    
     # 4) Devolver {"a": a, "b": b, "swap": swap, "done": False}.
-    #
-    # Cuando no queden pasos, devolvé {"done": True}.
-    return {"done": True}
+
+    return {"a": a, "b": b, "swap": swap, "done": False}
